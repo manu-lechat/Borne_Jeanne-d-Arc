fc6dc38005e31b1e8cc32582737243a6e609ac54
Manu Lechat <manu.lechat@free.fr>
2015-01-12 18:53:20 +0100

    + gestion de la video + gestion de l'autoplay

--- src/js/diaporama.js
+++ src/js/diaporama.js
@@ -1,20 +1,11 @@
 var currentId =0;
 var timer1;
-var speed_autoplay = 3000;
 
 var diaporama = function(){
 
-	this.diaporama_startAutoplay = function(){ 	
-	    timer1 = setTimeout( this.diaporam_next(), speed_autoplay);
-	}
+	// public functions ////////////////////////////////////////////////////////////////////////////////
 
-
-	this.diaporama_stopAutoplay = function(){ 	
-
-	    clearTimeout(timer1);
-	}
-
-
+	// To init Template 
 	this.diaporama_init = function(){ 	
 
 		// insert puces
@@ -23,75 +14,144 @@ var diaporama = function(){
 			document.getElementById('liste_puces').innerHTML +='<li>&#x25CF;</li>';
 		}
 
-		diaporam_showDiapo(0);
+		// play first Slide
+		diaporama_showDiapo(0);  
+	}
+
+	// To start autoplay
+	this.diaporama_startAutoplay = function(){ 	
+
+		startAutoplay(); 
+	}
+
+	// to call next slide
+	this.diaporama_next = function(){ 	
+
+		pause_autoplay(); 
+		next(); 	
+	}
+
+	this.diaporama_prev = function(){ 	
+
+		pause_autoplay(); 
+		prev();	
+	}
+
+	// private functions ////////////////////////////////////////////////////////////////////////////////
+
+
+	// autoplay --- 
+
+	function startAutoplay(){
+	    
+		clearTimeout(timer1);
+		timer1 = null;
+	    timer1 = setTimeout( loopAutoplay, 6000);
+	    console.log('setTimeout 6');
+	}
+
+	function loopAutoplay(){
+
+		startAutoplay();
+		next();
+	}
+
+	function pause_autoplay(){
+
+		clearTimeout(timer1);
+		timer1 = null;
+	    timer1 = setTimeout( loopAutoplay, 20000);	    
+	    // autoplay will start after 20' of inactivity
+	    console.log('setTimeout 20');
+	}
+
+	function stop_autoplay(){
+
+		clearTimeout(timer1);
+		timer1 = null;
 	}
 
 
-	this.diaporam_next = function(){ 	
+	// controls --- 
 
-		console.log("diaporam_next");
+	function next(){
+
 		if(currentId<JsonArray.items.length-1){ currentId++;  }else{ currentId = 0; }
-		diaporam_showDiapo(currentId);
+		diaporama_showDiapo(currentId);
 	}
 
+	function prev(){ 	
 
-	this.diaporam_prev = function(){ 	
-
-		console.log("diaporam_prev");
 		if(currentId>0){ currentId--; }else{ currentId=JsonArray.items.length-1; }
-		diaporam_showDiapo(currentId);
+		diaporama_showDiapo(currentId);
 	}
 
-
-	function diaporam_showDiapo(id){
+	function diaporama_showDiapo(id){
 
 		var currentId = id;
 		var currentItem = JsonArray.items[id];
-		// console.log("show diapo num "+id);
-		// console.log(JsonArray.items[id].titre_fr);
-		//TweenLite.to(".content_fr", 0.2, { opacity:"0", onComplete: function(){ 
 
-			// txt fr
-			document.getElementById('titre_fr').innerHTML = currentItem.titre_fr;
-			document.getElementById('txt_fr').innerHTML = currentItem.txt_fr;
-			document.getElementById('auteur_fr').innerHTML = currentItem.auteur_fr;
-			document.getElementById('source_fr').innerHTML = currentItem.source_fr;
-			// txt en
-			document.getElementById('titre_en').innerHTML = JsonArray.items[id].titre_en;
-			document.getElementById('txt_en').innerHTML = JsonArray.items[id].txt_en;
-			document.getElementById('auteur_en').innerHTML = JsonArray.items[id].auteur_en;
-			document.getElementById('source_en').innerHTML = JsonArray.items[id].source_en;
+		// txt fr
+		document.getElementById('titre_fr').innerHTML = currentItem.titre_fr;
+		document.getElementById('txt_fr').innerHTML = currentItem.txt_fr;
+		document.getElementById('auteur_fr').innerHTML = currentItem.auteur_fr;
+		document.getElementById('source_fr').innerHTML = currentItem.source_fr;
+		// txt en
+		document.getElementById('titre_en').innerHTML = JsonArray.items[id].titre_en;
+		document.getElementById('txt_en').innerHTML = JsonArray.items[id].txt_en;
+		document.getElementById('auteur_en').innerHTML = JsonArray.items[id].auteur_en;
+		document.getElementById('source_en').innerHTML = JsonArray.items[id].source_en;
 
-
-		/* } });
-		TweenLite.to(".content_en", 0.2, { opacity:"0"});
-		TweenLite.to(".content_fr", 0.2, { delay:0.2, opacity:"1" });
-		TweenLite.to(".content_en", 0.2, { delay:0.2, opacity:"1" });
-		*/
 		// puces
 		$("#liste_puces li" ).removeClass("active");
 		document.getElementById("liste_puces").children[id].className = "active";
 
 		// img / videos
-		$("#media_container div").each(function() {	this.className = "divToHide"; });
 
-		if(currentItem.type_media=="video"){}
-		else{
+		$("#media_container div.divToKill_first").each(function() { this.parentNode.removeChild(this); });
 
-			document.getElementById("media_container").innerHTML += "<div class='div_to_show'><img  src='media/"+JsonArray.items[id].file+"'><div>"
+		if(currentItem.type_media!="video"){
+
+			// it's a jpg
+			console.log('insert jpg');
+			// on marque les div existantes pour pouvoir les effacer par la suite
+			$("#media_container div").each(function() {	this.className = "divToKill"; });
+			// on insère une div contenant l'image à afficher
+			document.getElementById("media_container").innerHTML += "<div class='div_to_show'><img  src='media/"+JsonArray.items[id].file+"'></div>"
+			// on tween en opacity de 0 à 1 - onComplete, on vire les div marquées
+			TweenLite.fromTo("#media_container .div_to_show", 3, { opacity:"0"},{ opacity:"1", onComplete: function(){
+				$("#media_container div.divToKill").each(function() { this.parentNode.removeChild(this); });
+				console.log("delete divToKill");
+			}});
+		}else{
+			// it's a video
+			console.log('insert video');
+			// 1 - stop autoplay
+			stop_autoplay();
+			//  2- on gère la dispartition du contenu précédent
+			$("#media_container div").each(function() {	this.className = "divToKill"; });
+			TweenLite.to("#media_container .divToKill", 1, { opacity:"0", onComplete: function(){
+				$("#media_container div.divToKill").each(function() { this.parentNode.removeChild(this); });
+				console.log("delete divToKill");
+			}});
+			// 3 - on insère une div contenant la vidéo à afficher
+			var htmlToInsert = "<div class='div_to_show divToKill_first'>";
+			htmlToInsert += "	<video id='videoclip' controls='Play, Pause, Seeking' autoplay>";
+			htmlToInsert += "	<source  src='media/"+JsonArray.items[id].file+"' type='video/mp4'>";
+			htmlToInsert += "	</video>";
+			htmlToInsert += "</div>";
+			document.getElementById("media_container").innerHTML += htmlToInsert;
+			TweenLite.to("#media_container .div_to_show", 2, { opacity:"1" });
+			// 4 - on gère la detection de la fin de la vidéo
+			var toDoOnEnd = function() { 
+				startAutoplay(); ;	
+				next();
+			}
+			document.getElementById("videoclip").addEventListener("ended", toDoOnEnd);
+
 
 		}
 
-		TweenLite.fromTo("#media_container .div_to_show", 1, { opacity:"0"},{ opacity:"1", onComplete: function(){
-
-			$("#media_container div.divToHide").each(function() { this.parentNode.removeChild(this); });
-		}} );
-
-	}
-
-
-	function change_coloneGauche_content(){
-		
 	}
 
 
--- src/js/main.js
+++ src/js/main.js
@@ -4,12 +4,11 @@ var currentStep = 0;
 var urlJson = "data/borne_JA.json";
 var obj_diaporama;
 var timer_autoplay;
+
 function initMain(){
 
-	console.log("init main");
+	console.log("initMain()");
 	doNextStep();
-// ("data/borne_JA.json"
-	//initDiaporama();
 }
 
 
@@ -18,59 +17,29 @@ function doNextStep(){
 	currentStep++;
 	switch(currentStep){
 		case 1: 
-			console.log("step1");
 			get_json();		
 		break;	
 		case 2: 	
 			obj_diaporama = new diaporama();
 			obj_diaporama.diaporama_init();
-			doNextStep();
-		break;
-		case 3: 	
+			obj_diaporama.diaporama_startAutoplay();
 			init_listeners();
-			init_setTimeOut_autoplay();
 		break;
 	}
 }
 
-
+// LISTENERS ////////////////////////////////////////////////////////////////////////////////////////////
 
 function init_listeners(){  console.log("init_listeners()");
 
-
 	document.getElementById('arrow_prev').onclick = function() { 
-		obj_diaporama.diaporama_stopAutoplay();	
-		obj_diaporama.diaporam_prev();
-		init_setTimeOut_autoplay();
+		obj_diaporama.diaporama_prev();
 	};
 
 	document.getElementById('arrow_next').onclick = function() { 
-		obj_diaporama.diaporama_stopAutoplay();	
-		obj_diaporama.diaporam_next();	
-		init_setTimeOut_autoplay();
+		obj_diaporama.diaporama_next();	
 	};
 }
-
-
-function init_setTimeOut_autoplay(){
-
-
-    clearTimeout(timer_autoplay);
-	timer_autoplay = setTimeout( start_diaporama_autoplay(), 10000);
-
-}
-
-function start_diaporama_autoplay(){
-
-	console.log("start autoplay");
-	obj_diaporama.diaporama_startAutoplay();
-}
-
-
-
-
-
-
 
 // JSON ////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -86,11 +55,7 @@ function useJsonDatas(arr) {
 	doNextStep();
 }
 
-// JSON ////////////////////////////////////////////////////////////////////////////////////////////
-
-
-
-
+// RESET ////////////////////////////////////////////////////////////////////////////////////////////
 
 function main_reset(){
 	window.location.reload();
